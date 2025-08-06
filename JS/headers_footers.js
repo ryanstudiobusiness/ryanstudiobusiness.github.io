9d500509cc412ef23bf18a775f0097597fb5c9a2
yanya <yanyan2019ry@gmail.com>
2025-08-06 10:39:55 +0800

    file reformatting

---
+++ JS/headers_footers.js
@@ -0,0 +1,16 @@
+function loadHeaderFooter() {
+  fetch('../HTML/header.html')
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById('top-bar').innerHTML = html;
+    })
+    .catch(err => console.error('Header load failed:', err));
+
+  fetch('../HTML/footer.html')
+    .then(res => res.text())
+    .then(html => {
+      document.getElementById('footer').innerHTML = html;
+    })
+    .catch(err => console.error('Footer load failed:', err));
+}
+loadHeaderFooter();
