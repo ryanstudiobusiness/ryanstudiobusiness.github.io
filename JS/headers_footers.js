14bc7baeeb3147cd42f9a04ba5133a99bc9f968f
RyanStudioo <yanyan2019ry@gmail.com>
2025-08-10 07:05:52 +0800

    test fix

--- JS/headers_footers.js
+++ JS/headers_footers.js
@@ -1,12 +1,12 @@
 function loadHeaderFooter() {
-  fetch('../HTML/header.html')
+  fetch('HTML/header.html')
     .then(res => res.text())
     .then(html => {
       document.getElementById('top-bar').innerHTML = html;
     })
     .catch(err => console.error('Header load failed:', err));
 
-  fetch('../HTML/footer.html')
+  fetch('HTML/footer.html')
     .then(res => res.text())
     .then(html => {
       document.getElementById('footer').innerHTML = html;
