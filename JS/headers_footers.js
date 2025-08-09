81d13e184da4703a9134628623f6ff383bda6911
RyanStudioo <yanyan2019ry@gmail.com>
2025-08-10 07:12:31 +0800

    basepath

--- JS/headers_footers.js
+++ JS/headers_footers.js
@@ -1,12 +1,13 @@
 function loadHeaderFooter() {
-  fetch('HTML/header.html')
+  const basePath = window.location.pathname.substring(0, window.location.pathname.lastIndexOf('/') + 1);
+  fetch(basePath+ 'HTML/header.html')
     .then(res => res.text())
     .then(html => {
       document.getElementById('top-bar').innerHTML = html;
     })
     .catch(err => console.error('Header load failed:', err));
 
-  fetch('HTML/footer.html')
+  fetch(basePath+'HTML/footer.html')
     .then(res => res.text())
     .then(html => {
       document.getElementById('footer').innerHTML = html;
